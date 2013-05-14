c28a0d46fe55fe1c7c6cb4ded6e17fbe799388db
Alex Ianus <aianus@uwaterloo.ca>
2013-05-14 14:27:45 -0400

    Minor modifications of makefile

--- src/arm.mk
+++ src/arm.mk
@@ -1,7 +1,7 @@
 CC		= /u/wbcowan/gnuarm-4.0.2/arm-elf/bin/gcc
 AS		= /u/wbcowan/gnuarm-4.0.2/arm-elf/bin/as
 LD		= /u/wbcowan/gnuarm-4.0.2/arm-elf/bin/ld
-CFLAGS	= -c -fPIC -fno-builtin -Wall -I. -I../include -mcpu=arm920t -msoft-float
+CFLAGS	= -c -g -fPIC -fno-builtin -Wall -I. -I../include -mcpu=arm920t -msoft-float
 # -g: include hooks for gdb
 # -c: only compile
 # -mcpu=arm920t: generate code for the 920t architecture
@@ -11,4 +11,4 @@ CFLAGS	= -c -fPIC -fno-builtin -Wall -I. -I../include -mcpu=arm920t -msoft-float
 ASFLAGS	= -mcpu=arm920t -mapcs-32
 # -mapcs: always generate a complete stack frame
 
-LDFLAGS = -init main -Map a0.map -N -T orex.ld  -L/u/wbcowan/gnuarm-4.0.2/lib/gcc/arm-elf/4.0.2 -L../lib
+LDFLAGS = -init main -Map kernel.map -N -T orex.ld  -L/u/wbcowan/gnuarm-4.0.2/lib/gcc/arm-elf/4.0.2 -L../lib
