5f1ca493e7d102cde4149113cdf89836554ea337
Avi Itskovich <avi.itskovich@gmail.com>
2013-05-16 12:10:18 -0400

    Use real register names

--- src/switch.c
+++ src/switch.c
@@ -8,11 +8,11 @@ static void *ksp = 0;
 void kernel_exit(Task *task) {
     // Push the registers onto the kernel stack.
     asm(
-        "stmfd sp!, {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r14, r15}" "\t\n"
+        "stmfd sp!, {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, sl, fp, ip, lr, pc}" "\t\n"
         "str sp, %[kernel_stack_pointer]" "\t\n"
         "mov sp, %[user_stack_pointer]" "\t\n"
         "msr spsr, %[user_status_register]" "\t\n"
-        "ldmfd sp!, {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r14, r15}" "\t\n"
+        "ldmfd sp!, {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, sl, fp, ip, lr, pc}" "\t\n"
         : [kernel_stack_pointer] "=m" (ksp)
         : [user_stack_pointer] "r" (task->sp),
           [user_status_register] "r" (task->spsr)
