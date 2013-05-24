3b1e9cc68ef287de21731ae14738777c183bc7d8
Avi Itskovich <avi.itskovich@gmail.com>
2013-05-24 15:27:07 -0400

    Add BWIO Header Guard

--- include/bwio.h
+++ include/bwio.h
@@ -2,6 +2,9 @@
  * bwio.h
  */
 
+#ifndef _BWIO_H_
+#define _BWIO_H_
+
 typedef char *va_list;
 
 #define __va_argsiz(t)	\
@@ -37,3 +40,5 @@ int bwputr( int channel, unsigned int reg );
 void bwputw( int channel, int n, char fc, char *bf );
 
 void bwprintf( int channel, char *format, ... );
+
+#endif
