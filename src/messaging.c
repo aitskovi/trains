bb040c5c9c4eee2fccb5629a66ddb469e7ddc1aa
Avi Itskovich <avi.itskovich@gmail.com>
2013-05-26 19:50:42 -0400

    Messaging Part 2

--- src/messaging.c
+++ src/messaging.c
@@ -7,87 +7,81 @@
 
 #define min(a,b) (a) < (b) ? (a) : (b)
 
-struct message_queue {
-    struct circular_queue src_queue;    /* The src tid that sent you the message */
-    struct circular_queue data_queue;   /* The pieces of data */
-    struct circular_queue len_queue;    /* The lengths */
-};
+// A queue of messages waiting for you.
+static struct circular_queue mailboxes[MAX_TASKS];
 
-int message_queue_empty(struct message_queue *queue) {
-    int empty_src = circular_queue_empty(&(queue->src_queue));
-    int empty_data = circular_queue_empty(&(queue->data_queue));
-    int empty_len = circular_queue_empty(&(queue->len_queue));
-
-    // TODO: Check that all empties are the same.
-
-    return empty_src && empty_data && empty_len;
-}
-
-int message_queue_pop(struct message_queue *queue, int *src, char **data, int *len) {
-    *src = (int)circular_queue_pop(&(queue->src_queue));
-    *data = (char*)circular_queue_pop(&(queue->data_queue));
-    *len = (int)circular_queue_pop(&(queue->len_queue));
-
-    return 0;
-}
-
-int message_queue_push(struct message_queue *queue, int src, char *data, int len) {
-    circular_queue_push(&(queue->src_queue), (void *)src);
-    circular_queue_push(&(queue->data_queue), (void *)data);
-    circular_queue_push(&(queue->len_queue), (void *)len);
-
-    return 0;
-}
-
-// A circular queue of messages you're recieving.
-static struct message_queue message_queues[MAX_TASKS];
+// An array of messages you've sent.
+static char *msgs[MAX_TASKS];
+static int msg_lengths[MAX_TASKS];
 
 // An array of replies you're set to recieve.
 static char* replies[MAX_TASKS];
 static int reply_lengths[MAX_TASKS];
 
-int ksend(int src_tid, int dst_tid, char *msg, int msglen, char *reply, int replylen) {
-    //TODO(Add Valid TID Check)
-    //TODO(Add Running Task Check)
-
-    int error = message_queue_push(&(message_queues[dst_tid]), src_tid, msg, msglen);
-    if (error) {
-        bwprintf(COM2, 
-                "Placing a message onto the message queue for task: %d, failed",
-                dst_tid);
+void initialize_messaging() {
+    int i;
+    for (i = 0; i < MAX_TASKS; ++i) {
+        circular_queue_initialize(&mailboxes[i]);
+        msgs[i] = 0;
+        msg_lengths[i] = 0;
+        replies[i] = 0;
+        reply_lengths[i] = 0;
     }
+}
 
+int ksend(int src, int dst, char *msg, int msglen, char *reply, int replylen) {
+    // Put your send buffer in place, and wait.
+    msgs[src] = msg;
+    msg_lengths[src] = msglen;
 
-    replies[src_tid] = reply;
-    reply_lengths[src_tid] = replylen;
+    // Put your reply buffer in place, and wait.
+    replies[src] = reply;
+    reply_lengths[src] = replylen;
 
-    // TODO Attempt to unblock tasks.
+    // Tell the task he has a message from you.
+    int error = circular_queue_push(&mailboxes[dst], (void *)src);
+    if (error) {
+        bwprintf(COM2, "Error Informing Task: %d of Message from %d", dst, src);
+        return -1;
+    }
 
     return 0;
 }
 
-int krecieve(int dst_tid, int *src_tid, char *msg, int msglen) {
-    //TODO(Add Valid TID check for dst_tid)
-    struct message_queue *mqueue = &(message_queues[dst_tid]);
+int krecieve(int dst, int *src, char *msg, int msglen) {
+    struct circular_queue *mailbox = &mailboxes[dst];
 
-    if (message_queue_empty(mqueue)) {
-        // BLOCK
+    // Check if we have a messsage waiting for us.
+    if (circular_queue_empty(mailbox)) {
         bwprintf(COM2, "No Messages!, Let's block\n");
         return -1;
     }
 
     // Grab the message.
-    char *data;
-    int len;
-    int error = (int)message_queue_pop(mqueue, src_tid, &data, &len);
-    if (error) {
-        bwprintf(COM2, "Error popping message of queue");
-        return -2;
-    }
+    *src = (int)circular_queue_pop(mailbox);
 
     // Copy the data into the correct buffer.
-    msglen = min(msglen, len);
-    memcpy(msg, data, msglen);
+    msglen = min(msglen, msg_lengths[*src]);
+    memcpy(msg, msgs[*src], msglen);
+
+    // Remove the message we've consume.
+    msg_lengths[*src] = 0;
+    msgs[*src] = 0;
 
     return msglen;
+}
+
+int kreply(int tid, char *reply, int replylen) {
+    // Ensure we're waiting for a reply, and not send blocked or not blocked at all.
+    if (msgs[tid] != 0 && reply_lengths !=0) return -1;
+    if (replies[tid] == 0 && reply_lengths[tid] == 0) return -2;
+
+    // Write data into the waiting task's buffer.
+    replylen = min(replylen, reply_lengths[tid]);
+    memcpy(replies[tid], reply, replylen);
+
+    replies[tid] = 0;
+    reply_lengths[tid] = 0;
+
+    return replylen;
 }
