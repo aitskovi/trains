22dfef14e28caed50d4bdc2edf7068ae4c9b4528
Alex Ianus <aianus@uwaterloo.ca>
2013-05-28 18:07:16 -0400

    Renamed confusing variables

--- src/messaging.c
+++ src/messaging.c
@@ -7,41 +7,41 @@
 
 #define min(a,b) (a) < (b) ? (a) : (b)
 
-// A queue of messages waiting for you.
+// A queue of tids that have sent messages a to given thread
 static struct circular_queue mailboxes[MAX_TASKS];
 
-// An array of messages you've sent.
-static char *msgs[MAX_TASKS];
-static int msg_lengths[MAX_TASKS];
+// Buffers used for sending
+static char *send_buffers[MAX_TASKS];
+static int send_buffer_lengths[MAX_TASKS];
 
-// An array of message you're set to recieve.
-static char* rcvds[MAX_TASKS];
-static int rcvd_lengths[MAX_TASKS];
-static int* rcvd_srcs[MAX_TASKS];
+// Buffers used for receiving
+static char* receive_buffers[MAX_TASKS];
+static int receive_buffer_lengths[MAX_TASKS];
+static int* receive_buffer_sources[MAX_TASKS];
 
 void initialize_messaging() {
     int i;
     for (i = 0; i < MAX_TASKS; ++i) {
         circular_queue_initialize(&mailboxes[i]);
-        msgs[i] = 0;
-        msg_lengths[i] = 0;
-        rcvds[i] = 0;
-        rcvd_lengths[i] = 0;
-        rcvd_srcs[i] = 0;
+        send_buffers[i] = 0;
+        send_buffer_lengths[i] = 0;
+        receive_buffers[i] = 0;
+        receive_buffer_lengths[i] = 0;
+        receive_buffer_sources[i] = 0;
     }
 }
 
 int msg_send(int src, int dst, char *msg, int msglen, char *reply, int replylen) {
-    if (msgs[src] != 0 || msg_lengths[src] != 0) return -1;
-    if (rcvds[src] != 0 || rcvd_lengths[src] != 0) return -2;
+    if (send_buffers[src] != 0 || send_buffer_lengths[src] != 0) return -1;
+    if (receive_buffers[src] != 0 || receive_buffer_lengths[src] != 0) return -2;
 
     // Put your send buffer in place, and wait.
-    msgs[src] = msg;
-    msg_lengths[src] = msglen;
+    send_buffers[src] = msg;
+    send_buffer_lengths[src] = msglen;
 
     // Put your reply buffer in place, and wait.
-    rcvds[src] = reply;
-    rcvd_lengths[src] = replylen;
+    receive_buffers[src] = reply;
+    receive_buffer_lengths[src] = replylen;
 
     // Tell the task he has a message from you.
     int error = circular_queue_push(&mailboxes[dst], (void *)src);
@@ -58,22 +58,22 @@ int msg_recieve(int dst, int *src, char *msg, int msglen) {
 
     if (src == 0 && msg == 0 && msglen == 0) {
         // TODO Assert that we have something in rcvds and rcvd_length
-        msg = rcvds[dst];
-        msglen = rcvd_lengths[dst];
-        src = rcvd_srcs[dst];
-        rcvds[dst] = 0;
-        rcvd_lengths[dst] = 0;
-        rcvd_srcs[dst] = 0;
+        msg = receive_buffers[dst];
+        msglen = receive_buffer_lengths[dst];
+        src = receive_buffer_sources[dst];
+        receive_buffers[dst] = 0;
+        receive_buffer_lengths[dst] = 0;
+        receive_buffer_sources[dst] = 0;
     }
 
     // Assert that we don't have anything left over in our rcvds buffer.
 
-    // Check if we have a messsage waiting for us.
+    // Check if we have a message waiting for us.
     if (circular_queue_empty(mailbox)) {
         // We're about to block. Let's store our buffer.
-        rcvds[dst] = msg;
-        rcvd_lengths[dst] = msglen;
-        rcvd_srcs[dst] = src;
+        receive_buffers[dst] = msg;
+        receive_buffer_lengths[dst] = msglen;
+        receive_buffer_sources[dst] = src;
         return -1;
     }
 
@@ -81,27 +81,27 @@ int msg_recieve(int dst, int *src, char *msg, int msglen) {
     *src = (int)circular_queue_pop(mailbox);
 
     // Copy the data into the correct buffer.
-    msglen = min(msglen, msg_lengths[*src]);
-    memcpy(msg, msgs[*src], msglen);
+    msglen = min(msglen, send_buffer_lengths[*src]);
+    memcpy(msg, send_buffers[*src], msglen);
 
     // Remove the message we've consume.
-    msg_lengths[*src] = 0;
-    msgs[*src] = 0;
+    send_buffer_lengths[*src] = 0;
+    send_buffers[*src] = 0;
 
     return msglen;
 }
 
 int msg_reply(int tid, char *reply, int replylen) {
     // Ensure we're waiting for a reply, and not send blocked or not blocked at all.
-    if (msgs[tid] != 0 || msg_lengths[tid] !=0) return -1;
-    if (rcvds[tid] == 0 || rcvd_lengths[tid] == 0) return -2;
+    if (send_buffers[tid] != 0 || send_buffer_lengths[tid] !=0) return -1;
+    if (receive_buffers[tid] == 0 || receive_buffer_lengths[tid] == 0) return -2;
 
     // Write data into the waiting task's buffer.
-    replylen = min(replylen, rcvd_lengths[tid]);
-    memcpy(rcvds[tid], reply, replylen);
+    replylen = min(replylen, receive_buffer_lengths[tid]);
+    memcpy(receive_buffers[tid], reply, replylen);
 
-    rcvds[tid] = 0;
-    rcvd_lengths[tid] = 0;
+    receive_buffers[tid] = 0;
+    receive_buffer_lengths[tid] = 0;
 
     return replylen;
 }
