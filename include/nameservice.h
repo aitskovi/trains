c686e253252b8132c97c4c3f1f6dc66fc8683aa0
Avi Itskovich <avi.itskovich@gmail.com>
2013-06-01 13:44:57 -0400

    Remove static memory from NameService
    We can now run multiple instances of the NameServer at the same time.

--- include/nameservice.h
+++ include/nameservice.h
@@ -1,10 +1,22 @@
 #ifndef _NAMESERVICE_H_
 #define _NAMESERVICE_H_
 
+#define MAX_NAME_LENGTH 10
+#define MAX_REGISTRATIONS 50
+
+struct Registration {
+    char name[MAX_NAME_LENGTH];
+    int tid;
+};
+
+struct NameService {
+    struct Registration registrations[MAX_REGISTRATIONS];
+};
+
 /**
  * Initialize the nameservice.
  */
-void initialize_nameservice();
+void nameservice_initialize(struct NameService *service);
 
 /**
  * Register a name with the nameservice.
@@ -13,13 +25,13 @@ void initialize_nameservice();
  * -1 Means Name was cut off.
  * -2 means Not Enough Slots
  */
-int nameservice_register(char *name, int tid);
+int nameservice_register(struct NameService *service, char *name, int tid);
 
 /**
  * Lookup a name in the nameservice.
  *
  * Returns either the tid or -1 for not found.
  */
-int nameservice_lookup(char *name);
+int nameservice_lookup(struct NameService *service, char *name);
 
 #endif
