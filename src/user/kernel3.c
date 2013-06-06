a270c981e402f0bbf0a546f7bfbb555e97a437c2
Avi Itskovich <avi.itskovich@gmail.com>
2013-06-06 19:52:39 -0400

    Add interrupt triggering

--- src/user/kernel3.c
+++ src/user/kernel3.c
@@ -1,20 +1,42 @@
+#include <log.h>
+#include <interrupt.h>
+#include <scheduling.h>
 #include <syscall.h>
 #include <ts7200.h>
-#include <log.h>
 
-#include <interrupt.h>
+void idle() {
+    log("Idle: Initializing\n");
+
+    for(;;) {
+    }
+}
 
 void first() {
     // Setup the timer interrupt.
 
+    log("First: Initializing\n");
+    log("First: Creating Idle Task\n");
+    Create(LOW, idle);
+    log("First: Created Idle Task\n");
+
+    log("First: Initializing Timer\n");
+
     enable_interrupt(TIMER_3_INTERRUPT);
 
-    int i;
-    for (i = 0; i < 3; ++i) {
-        log("Generating Interrupt\n");
-        generate_interrupt(TIMER_3_INTERRUPT);
-        log("Generated Interrupt\n");
-    }
+    int* ldr = (int *)(TIMER3_BASE + LDR_OFFSET);
+    int* val = (int *)(TIMER3_BASE + VAL_OFFSET);
+    int* crtl = (int *)(TIMER3_BASE + CRTL_OFFSET);
+
+    // Disable the clock.
+    *crtl = 1 * MODE_MASK;
+
+    // Set it's load value.
+    *ldr = 200;
+
+    // Start the clock.
+    *crtl = ENABLE_MASK + MODE_MASK;
+
+    log("First: Timer Initialized\n");
 
     Exit();
 }
