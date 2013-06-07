615c9d787186ae2d615359a215763a92d05e0f33
Alex Ianus <aianus@uwaterloo.ca>
2013-06-06 20:15:39 -0400

    Fixed alignment issues

--- src/libc/memcpy.c
+++ src/libc/memcpy.c
@@ -7,11 +7,18 @@
 
 
 #include <memcpy.h>
+#include <log.h>
 
 // Must be called with len % 4 == 0
 void *memcpy4(void *destination, void *source, unsigned int len) {
     int *dst = (int *) destination;
     int *src = (int *) source;
+
+    if ((unsigned int) destination % 4
+            || (unsigned int) source % 4
+            || len % 4) {
+        log("memcpy4 called with invalid arguments!\n");
+    }
 
     while (len) {
         *dst++ = *src++;
@@ -25,13 +32,23 @@ void *memcpy(void *destination, void *source, unsigned int len) {
     char *dst = (char *)destination;
     char *src = (char *)source;
 
-    while (len % 4) {
-        *dst++ = *src++;
-        len--;
+    if ((unsigned int) destination % 4
+            || (unsigned int) source % 4) {
+        log("Memcpy called with unaligned pointers!");
+    } else {
+        unsigned int bulk_size = len - len % 4;
+        memcpy4(destination, source, bulk_size);
+        dst += bulk_size;
+        src += bulk_size;
+        len -= bulk_size;
     }
 
-    // Now the length is a multiple of the word size
-    return memcpy4((void *) dst, (void *) src, len);
+    unsigned int i;
+    for (i = 0; i < len; ++i) {
+        *dst++ = *src++;
+    }
+
+    return destination;
 }
 
 /*
