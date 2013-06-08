5c9269f7ce9c7cb4f327902c35db914062d77fb2
Avi Itskovich <avi.itskovich@gmail.com>
2013-06-08 12:33:55 -0400

    Add better error handling to AwaitEvent

--- src/event.c
+++ src/event.c
@@ -1,6 +1,10 @@
 #include <event.h>
 
+#include <log.h>
 #include <task.h>
+
+#define INVALID_EVENT -1
+#define CORRUPTED_VOLATILE_DATA -2
 
 #define NO_WAITERS -1
 #define TOO_MANY_WAITERS -4
@@ -9,6 +13,10 @@ static Task *waiters[NUM_EVENTS];
 
 int kawait(Task *task, int event) {
    // Verify the event is valid.
+   if (event < 0 || event >= NUM_EVENTS) {
+       task_set_return_value(task, INVALID_EVENT);
+       return INVALID_EVENT;
+   }
 
    if (waiters[event]) return TOO_MANY_WAITERS;
 
@@ -19,7 +27,10 @@ int kawait(Task *task, int event) {
 }
 
 int kevent(int event, int data) {
-    if (!waiters[event]) return NO_WAITERS;
+    if (!waiters[event]) {
+        dlog("Recieved the event:%d, with no waiters\n", event);
+        return NO_WAITERS;
+    }
 
     Task *task = waiters[event];
     task_set_return_value(task, data);
